4170a91b396640958f1e0a4f24a0637a77971d22
Prerak126 <prerak126@gmail.com>
2017-08-15 13:56:02 -0400

    Simple 4

---
+++ 4.js
@@ -0,0 +1,35 @@
+var x = prompt("Enter a Year","2000");
+
+function Oscar(a) {
+    if(a==="2016"){
+        alert("Dabbang");
+    }
+
+    else if(a==="2015"){
+        alert("Bay Yaar");
+    }
+
+    else if(a==="2014"){
+        alert("Chello Divas");
+    }
+
+    else if(a==="2013"){
+        alert("Gujarat Lions");
+    }
+
+    else if(a==="2012"){
+        alert("Avengers");
+    }
+
+    else if(a==="2011"){
+        alert("Suryavansam");
+    }
+
+    else if(a==="2010") {
+        alert("Don");
+    }
+    else
+        alert("Prerak");
+}
+
+Oscar(x);
