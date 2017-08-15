8d1d6cbcb500f42faa3b0e352729794ab55442a6
Prerak126 <prerak126@gmail.com>
2017-08-15 13:55:17 -0400

    Simple 3

---
+++ 3.js
@@ -0,0 +1,33 @@
+var x = prompt("enter a day","Monday");
+
+function dish(a) {
+    if(a==="Monday"){
+        alert("Mondays, We serve Pasta");
+    }
+
+    else if(a==="Tuesday"){
+        alert("Tuesday, We serve Pasta");
+    }
+
+    else if(a==="Wednesday"){
+        alert("Wednesday, We serve Pasta");
+    }
+
+    else if(a==="Thursday"){
+        alert("Thursday, We serve Pasta");
+    }
+
+    else if(a==="Friday"){
+        alert("Friday, We serve Pasta");
+    }
+
+    else if(a==="Saturday"){
+        alert("Saturdays, We serve Pasta");
+    }
+
+    else if(a==="Sunday") {
+        alert("Sundays, We serve Pasta");
+    }
+}
+
+dish(x);
